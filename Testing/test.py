72cc066daffa86690f6c1751d4caf99ca66ec517
Emma Angus <emma.angus@my.simpson.edu>
2021-09-02 15:00:55 -0500

    work on testing

---
+++ Testing/test.py
@@ -0,0 +1,11 @@
+print("Most exciting class ever!")
+print("best class ever!")
+print("fun fun fun :)")
+print("paige said \"Hey\"!")
+print("what\nis\nup?")
+print("""Hey,
+How are
+you
+doing
+today?
+""")

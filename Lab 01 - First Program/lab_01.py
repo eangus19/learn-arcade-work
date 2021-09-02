7f39e63a99b5aff461cf5a06207d60417e62d797
Emma Angus <emma.angus@my.simpson.edu>
2021-09-02 15:26:20 -0500

    lab 1

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,6 @@
+print("Hello, my name is Emma Angus.")
+print("I am thinking of majoring in Computer Science and getting a minor in Computer Information Systems")
+print("""I compete
+in Cross Country. I enjoy running
+a lot! It is fun and keeps me in shape!!""")
+print("My mom has once said to me that \"you should always do something that you love.\"")
